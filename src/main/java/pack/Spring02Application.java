3cf38d5f1ccf4c52d3511334850cbb4c1a11f1a2
jimmminni <rlawlals024@gmail.com>
2025-08-06 09:12:15 +0900

    0911

--- src/main/java/pack/Spring02Application.java
+++ src/main/java/pack/Spring02Application.java
@@ -11,7 +11,7 @@ public class Spring02Application {
 
 	@GetMapping("/")
 	public String tiger() {
-		return "Spring02:1705";
+		return "Spring02:0911";
 	}
 	
 	public static void main(String[] args) {
